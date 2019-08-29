8396c2d2929edb480883ea84d4df61d499ee1769
maxtingle11 <54048496+maxtingle11@users.noreply.github.com>
2019-08-29 14:57:41 -0400

    Aug29Push

---
+++ module-1/morning-warm-up/week-1/1_1_dictionariesMax1_script.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Create a dictionary where each object contains a list of one float (Age) and one string (Family name) (at least 5 objects)
+# Example: {Charles: [23.4, "Darwin"], Alan: [42.5, "Turing"]}
+
+# In[10]:
+
+
+people = {'Sam': [27.45, 'Smith'], 'Beyonce': [37.37, 'Knowles'], 'Lady': [33.21, 'Gaga'], 'Kim': [27.11, 'Petras'], 'Adam': [37.54, 'Lambert']}
+people
+
+
+# 2. Delete one object from the dictionary
+
+# In[11]:
+
+
+del people["Adam"]
+people
+
+
+# 3. Replace the float number of one of your objects - we are changing a list entry inside a dictionary record! look at Darwin's new age
+# Example: {Charles: [99.73, "Darwin"], Alan: [42.5, "Turing"]}
+
+# In[12]:
+
+
+people['Kim'][0] = 34.33
+people
+
+
+# 4. write a for loop that goes through all records in the dictionary, prints the family name and assigns the float numbers into one merged list (see ages)
+# ages = [23.4, 22.9, 552.9]
+
+# In[32]:
+
+
+ages = []
+for person in people:
+    print(people[person][1])
+    ages.append(people[person][0])
+
+ages
+
+
+# 5. Download your notebbok as a .py (regular python script) save it somewhere you know
+
+# 6. Go to terminal, navigate to the folder where you have saved the script and execute it through the terminal
+# use commands: cd
+# python your_script.py
+# 
+# [optional] Calculate with a for loop the median and mean of the ages list
+
+# In[ ]:
+
+
+
+
